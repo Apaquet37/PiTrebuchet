b493bd55fad1690828e9910e96e81c0d05902496
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2021-03-23 11:52:41 -0400

    Update servo.py

--- servo.py
+++ servo.py
@@ -8,26 +8,26 @@ GPIO.setwarnings(False)
 GPIO.setup(servoPin, GPIO.OUT)
 
 pwm = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+pwm.start(2.5) # Initialization
 
 try:
   while True:
-    p.ChangeDutyCycle(5)
+    pwm.ChangeDutyCycle(5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
+    pwm.ChangeDutyCycle(7.5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
+    pwm.ChangeDutyCycle(10)
     time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
+    pwm.ChangeDutyCycle(12.5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
+    pwm.ChangeDutyCycle(10)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
+    pwm.ChangeDutyCycle(7.5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(5)
+    pwm.ChangeDutyCycle(5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
+    pwm.ChangeDutyCycle(2.5)
     time.sleep(0.5)
 except KeyboardInterrupt:
-  p.stop()
+  pwm.stop()
   GPIO.cleanup()
