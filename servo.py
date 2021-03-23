2c3a6915ff6483fa3f34c4abea0e4a315678aec4
Apaquet37 <apaquet37@charlottesvilleschools.org>
2021-03-23 15:24:39 +0000

    Working servo, now on to specific controls

--- servo.py
+++ servo.py
@@ -1,1 +1,18 @@
 #Code for a servo to adjust the angle of the release pin
+import RPi.GPIO as GPIO
+import time
+
+global pwm
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 50)
+pwm.start(0)
+
+
+while True:
+	GPIO.output(18,GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(18,GPIO.LOW)
+	time.sleep(1)
+	print("end")
