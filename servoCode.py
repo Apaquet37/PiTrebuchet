62bbfa7d0c90f07dafb788d471fc3a889beca014
Apaquet37 <apaquet37@charlottesvilleschools.org>
2021-05-04 17:03:01 +0100

    Simplified code for 2 servos to move back and forth in increments

--- servoCode.py
+++ servoCode.py
@@ -1,1 +1,16 @@
-from gpiozero.pins.pigpio.PiGPIOFactory import pigpiofactory
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(18)
+servo2 = Servo(21)
+
+while True:
+    servo.min()
+    servo2.min()
+    sleep(1)
+    servo.mid()
+    servo2.mid()
+    sleep(1)
+    servo.max()
+    servo2.max()
+    sleep(1)
