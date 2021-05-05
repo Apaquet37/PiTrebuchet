bb5317a6609c86435c9ba7849585344cc9d6bde4
Bhenry4 <82593996+Bhenry4@users.noreply.github.com>
2021-05-05 18:49:00 -0400

    Changed pin factory to gpiozero
    Changed pin factory to gpiozero to reduce servo jitter (requires pigpio daemon to be running)

--- servoCode.py
+++ servoCode.py
@@ -1,5 +1,8 @@
-from gpiozero import Servo
+from gpiozer.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo,Device
 from time import sleep
+
+Device.pin_factory = PiGPIOFactory()
 
 servo = Servo(18)
 servo2 = Servo(21)
