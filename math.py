95288c377eae8c8914487b609419749baa0226c4
Bhenry4 <82593996+Bhenry4@users.noreply.github.com>
2021-05-06 17:48:47 -0400

    Clarify comments

--- math.py
+++ math.py
@@ -1,16 +1,16 @@
 import math
 
-mCW = 1.16 #mass of cw, kg (2 washers weighing .58 each)
-g = 9.81 #gravity, Newtons 
-hCWI = 0.26 #initial height of cw, m
+mCW = 1.16 #mass of counterweight in kg (2 washers weighing .58 each)
+g = 9.81 #gravity in Newtons 
+hCWI = 0.26 #initial height of counterweight in m
 w = #rotational velocity (angular velocity?) in rad/sec
-hCWF = 0.03 #final height of cw, m
+hCWF = 0.03 #final height of counterweight in m
 mP = input("Mass of projectile:")#mass of projectile
 y = #height of projectile when released
 i = #moment of inertia
-r = 0.12 #distance from COM of cw to COR, m
+r = 0.12 #distance from COM of counterweight to COR in m
 
-vLCW = w*r #linear velocity of cw
+vLCW = w*r #linear velocity of counterweight
 
 vPF = math.sqrt((2((mCW*g*hCWI)-((1/2)*mCW*vLCW*vLCW)-((1/2)*i*w*w)-(mCW*g*hCWF)-(mP*g*y)))/mP)]
 
