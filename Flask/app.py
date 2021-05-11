a7d0ee1392efc7062a00482ead769829575fdb06
Apaquet37 <apaquet37@charlottesvilleschools.org>
2021-05-11 16:34:16 +0100

    Add servos to flask, need to fix indents

--- Flask/app.py
+++ Flask/app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo,Device
+from time import sleep
+
+Device.pin_factory = PiGPIOFactory()
+
+servo = Servo(18)
+servo2 = Servo(21)
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route("/", methods=["GET","POST"])
+def index():
+     if request.method == "POST":
+	servo.max()
+	servo2.max()
+	sleep(1)
+        msg = request.form.get("submitBtn")
+     else:
+	servo.min()
+	servo2.min()
+	msg = "No click yet."
+     return render_template("index.html", msg=msg)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=80)
+     app.run(host="0.0.0.0", port=80)
+
