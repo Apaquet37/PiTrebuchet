b47b7730a44cff0836bc40b95aa2e3247ac1fd04
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2021-05-18 11:50:13 -0400

    Add input back in

--- angleTest.py
+++ angleTest.py
@@ -8,8 +8,8 @@ servo = Servo(18)
 servo2 = Servo(21)
 
 while True:
-  #angle = input("Angle (between 1 and -1):")
-  servo.value = .75
+  angle = input("Angle (between 1 and -1):")
+  servo.value = angle
   sleep(1)
   servo.value = -1
   sleep(1)
