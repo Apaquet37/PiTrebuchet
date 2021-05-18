426fdc8fc283030526d17290fb4f2c6ae57f2133
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2021-05-18 11:36:16 -0400

    Update angleTest.py

--- angleTest.py
+++ angleTest.py
@@ -1,1 +1,15 @@
-import 
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo,Device
+from time import sleep
+
+Device.pin_factory = PiGPIOFactory()
+
+servo = Servo(18)
+servo2 = Servo(21)
+
+while True:
+  angle = input("Angle (between 1 and -1):")
+  servo.value(angle)
+  time.sleep(1)
+  servo.min()
+  time.sleep(1)
