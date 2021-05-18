ca2afaf28f08a099014cfb04951b3ec142d63281
Bhenry4 <82593996+Bhenry4@users.noreply.github.com>
2021-05-18 16:51:14 -0400

    Fixed Merge Conflicts

--- math.py
+++ math.py
@@ -1,18 +1,18 @@
 import math
 
-mCW = 1.16 #mass of counterweight in kg (2 washers weighing .58 each)
-g = 9.81 #gravity in Newtons 
-hCWI = 0.26 #initial height of counterweight in m
-w = None #rotational velocity (angular velocity?) in rad/sec
-hCWF = 0.03 #final height of counterweight in m
+mCW = 1.16 #mass of cw, kg (2 washers weighing .58 each)
+g = 9.81 #gravity, Newtons 
+hCWI = 0.26 #initial height of cw, m
+w = #rotational velocity (angular velocity?) in rad/sec
+hCWF = 0.03 #final height of cw, m
 mP = input("Mass of projectile:")#mass of projectile
-y = None #height of projectile when released
-i = None #moment of inertia
-r = 0.12 #distance from COM of counterweight to COR in m
+y = #height of projectile when released
+i = .0223161663 #moment of inertia, kg*m^2
+r = 0.12 #distance from COM of cw to COR, m
 
-vLCW = w*r #linear velocity of counterweight
+vLCW = w*r #linear velocity of cw
 
-vPF = math.sqrt((2((mCW*g*hCWI)-((1/2)*mCW*vLCW*vLCW)-((1/2)*i*w*w)-(mCW*g*hCWF)-(mP*g*y)))/mP)
+vPF = math.sqrt((2((mCW*g*hCWI)-((1/2)*mCW*vLCW*vLCW)-((1/2)*i*w*w)-(mCW*g*hCWF)-(mP*g*y)))/mP)]
 
 print(vPF)
 
