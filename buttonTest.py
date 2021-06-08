b1cbdc972062f0e4bad7c227894e7657321d4cc6
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2021-06-08 13:02:03 -0400

    comment button test

--- buttonTest.py
+++ buttonTest.py
@@ -14,14 +14,14 @@ servo2 = Servo(21)
 
 
 while True:
-  if GPIO.input(13) == GPIO.HIGH and buttonState == 0:
-    print("Button pressed")
-    angle = float(input("Angle (between 1 and -1):"))
-    buttonState = 1
-    servo1.value = angle
-    servo2.value = -1
-  if GPIO.input(13) == GPIO.LOW:
-    print("Button off")
-    servo1.value = 0
-    servo2.value = 1
-    buttonState = 0
+  if GPIO.input(13) == GPIO.HIGH and buttonState == 0: #Has the button been pressed?
+    print("Button pressed") #Print to the screen so I know
+    angle = float(input("Angle (between 1 and -1):")) #Ask the user to input an angle, convert that angle to a float
+    buttonState = 1 #A condition to make sure the loop only happens once per press
+    servo1.value = angle #Move the servo to the user inputted angle
+    servo2.value = -1 #Move a second servo to the other side
+  if GPIO.input(13) == GPIO.LOW: #When the button is not being pressed
+    print("Button off") #Print button off
+    servo1.value = 0 #Reset the servo
+    servo2.value = 1 #Move the other servo to the otehr side
+    buttonState = 0 #Reset the buttonstate
