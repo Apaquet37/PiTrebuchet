6d3c7cb94cc503ac1f74c09ed6420ebce0bc252b
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2021-06-09 18:46:58 -0400

    Add comments

--- angleTest.py
+++ angleTest.py
@@ -1,16 +1,17 @@
-from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero.pins.pigpio import PiGPIOFactory #All the necessary imports for the servos
 from gpiozero import Servo,Device
 from time import sleep
 
 Device.pin_factory = PiGPIOFactory()
 
-servo = Servo(18)
+servo = Servo(18) #Two servos, one on pin 18 and one on 21
 servo2 = Servo(21)
 
-while True:
-  angle = float(input("Angle (between 1 and -1):"))
-  print(angle)
-  servo.value = angle
-  sleep(3)
-  servo.value = 0
-  sleep(1)
+while True: #Forever loop
+  angle = float(input("Angle (between 1 and -1):")) #Prompt the user to input an angle and convert it to a float
+  print(angle) #Print the angle just to make sure the code is working
+  servo.value = angle #Move the first servo to the angle
+  sleep(3) #Wait for 3 seconds
+  servo.value = 0 #Move the servo back to zero (the middle, 90ish degrees)
+  sleep(1) #Wait for 1 second before starting the loop again
+ 
